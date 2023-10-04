8318852c199ceae0cb75b4b6d345ca0e4b205124
nadjaob <nadja.oblaender@gmx.de>
2023-10-04 14:20:23 +0200

    installed django and added gitignore file

---
+++ users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+# class User(AbstractUser):
+#   bio = models.TextField(max_length=300, blank=True, null=True)
+#   # profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
