42fa033602e884da9d883a5a18ad977e87e04145
nadjaob <nadja.oblaender@gmx.de>
2023-10-13 15:53:38 +0200

    fixed search function in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -58,6 +58,7 @@ export default function Header({ setRenderApp, renderApp, userImage, username })
         console.log(error.message)
       }
     }
+    // getDestinations()
     if (destinations) {
       getDestinations()
     }
